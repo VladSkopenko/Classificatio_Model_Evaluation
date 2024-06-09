146d158b24c894650f0c4c096bfd29f74c33bfdf
VladSkopenko <skopirka2k17@gmail.com>
2024-06-09 21:50:25 +0300

    add cross_entopy_m

--- cross_entropy.py
+++ cross_entropy.py
@@ -18,3 +18,14 @@ def cross_entropy(targets, predictions):
             merge = -1 * (first_part + second_part)
             losses.append(merge)
         return np.sum(losses) / len(targets)
+
+
+def cross_entropy_m(targets, predictions):
+    if len(targets) != len(predictions):
+        print("Length of targets and predictions should be same")
+    else:
+        m = len(targets)
+        first_part = np.dot(targets.T, np.log(predictions))
+        second_part = np.dot((1 - targets).T, np.log(1 - predictions))
+        ce = -(first_part + second_part) / m
+        return ce
