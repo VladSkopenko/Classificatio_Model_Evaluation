f09e17a862ed25eedbfac9481a4da1e500709d1e
VladSkopenko <skopirka2k17@gmail.com>
2024-06-10 14:10:25 +0300

    update logistic regression sklearn

--- logisctic_regresiion_SKLEARN.py
+++ logisctic_regresiion_SKLEARN.py
@@ -1,8 +1,12 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
 
-
-X, Y = load_iris(return_X_y=True)
-clf = LogisticRegression(max_iter=1000).fit(X, Y)
-result = clf.score(X, Y)
+X, y = load_iris(return_X_y=True)
+clf = LogisticRegression(max_iter=1000).fit(X, y)
+result = clf.score(X, y)
 print(result)
+clss = clf.predict(X)  # значення
+y_predict = clf.predict_proba(X)  # ймовірності
+
+print(log_loss(y, y_predict))
